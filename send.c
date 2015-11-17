e62f1c95eeeffaf41bfe6f53a8d6c9146757348e
cse533-7 Student <cse533-7@minix.cs.sunysb.edu>
2015-11-16 20:24:31 -0500

    Send Recv functions

--- send.c
+++ send.c
@@ -1,10 +1,18 @@
-#include "A3.h"
+#include "a3.h"
 
-msg_send(int sockfd, char* dest_ip, int dest_port, char* msg, int flag)
+void msg_send(int sockfd, char* dest_ip, int dest_port, char* msg, int flag)
 {
 
-
-
-
+	char buff[MAXLINE+1];
+	
+	/* Format of buff --> destn_ip dest_port flag message */
+	strcat(buff,dest_ip);
+	strcat(buff," ");
+	sprintf(buff,"%d %d ",dest_port,flag);
+	strcat(buff,msg);
+	
+	Send( sockfd, buff, strlen(buff)+1, 0 );
+	
+	printf("SEND_MSG: %s\n", buff);	
 
 }
